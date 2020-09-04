02dec704ac7e6bf0cdf3b9eae0d870fc3730ae73
Yingqi Zhang <yzha0464@student.monash.edu>
2020-09-04 14:10:29 +1000

    home page chat change

--- foodary/App_Start/BundleConfig.cs
+++ foodary/App_Start/BundleConfig.cs
@@ -25,6 +25,8 @@ namespace foodary
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+      
         }
     }
 }
+
